8baa3022de00c331bef182c91495051951f9defe
ViolinSolo <i.violinsolo@gmail.com>
2019-04-24 11:08:38 +0800

    add setup

--- setup.py
+++ setup.py
@@ -2,57 +2,150 @@
 # -*- coding: utf-8 -*-
 # Author: violinsolo
 # Created on 2019/4/11
+
+# Note: To use the 'upload' functionality of this file, you must:
+#   $ pip install twine
+
+import io
 import os
+import sys
+from shutil import rmtree
 
-# 需要将那些包导入
-packages = ['sci-util', ]
+from setuptools import find_packages, setup, Command
 
-# 导入静态文件
-file_data = [
-    # ("sci-util/static", ["sci-util/static/icon.svg", "sci-util/static/config.json"]),
-]
+# Package meta-data.
+NAME = 'sci-util'
+DESCRIPTION = 'Sci utils for science calculation.'
+URL = 'https://github.com/iViolinSolo/python-util'
+EMAIL = 'i.violinsolo@gmail.com'
+AUTHOR = 'ViolinSolo'
+REQUIRES_PYTHON = '>=3.6.0'  # ">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3*",  # Python版本依赖
+VERSION = None
+
+
+# # 导入静态文件
+# file_data = [
+#     # ("sci-util/static", ["sci-util/static/icon.svg", "sci-util/static/config.json"]),
+# ]
 
 # 第三方依赖
-requires = [
+# What packages are required for this module to be executed?
+REQUIRED = [
     'pandas>=0.23.4',
     'numpy'
+    # 'requests', 'maya', 'records',
 ]
 
-here = os.path.dirname(os.path.realpath(__file__))
-# 自动读取version信息
+# What packages are optional?
+EXTRAS = {
+    # 'fancy feature': ['django'],
+}
+
+# The rest you shouldn't have to touch too much :)
+# ------------------------------------------------
+# Except, perhaps the License and Trove Classifiers!
+# If you do change the License, remember to change the Trove Classifier for that!
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+# Import the README and use it as the long-description.
+# Note: this will only work if 'README.md' is present in your MANIFEST.in file!
+try:
+    with io.open(os.path.join(here, 'README.md'), encoding='utf-8') as f:
+        long_description = '\n' + f.read()
+except FileNotFoundError:
+    long_description = DESCRIPTION
+
+# Load the package's __version__.py module as a dictionary.
 about = {}
-with open(os.path.join(here, 'sci-util', '__version__.py'), 'r', encoding='utf-8') as f:
-    exec(f.read(), about)
+if not VERSION:
+    project_slug = NAME.lower().replace("-", "_").replace(" ", "_")
+    with open(os.path.join(here, project_slug, '__version__.py')) as f:
+        exec(f.read(), about)
+else:
+    about['__version__'] = VERSION
 
-# 自动读取readme
-with open('README.rst', 'r', encoding='utf-8') as f:
-    readme = f.read()
 
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = 'Build and publish the package.'
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print('\033[1m{0}\033[0m'.format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status('Removing previous builds…')
+            rmtree(os.path.join(here, 'dist'))
+        except OSError:
+            pass
+
+        self.status('Building Source and Wheel (universal) distribution…')
+        os.system('{0} setup.py sdist bdist_wheel --universal'.format(sys.executable))
+
+        self.status('Uploading the package to PyPI via Twine…')
+        os.system('twine upload dist/*')
+
+        self.status('Pushing git tags…')
+        os.system('git tag v{0}'.format(about['__version__']))
+        os.system('git push --tags')
+
+        sys.exit()
+
+
+# Where the magic happens:
 setup(
-    name=about["__title__"],  # 包名称
-    version=about["__version__"],  # 包版本
-    description=about["__description__"],  # 包详细描述
-    long_description=readme,   # 长描述，通常是readme，打包到PiPy需要
-    author=about["__author__"],  # 作者名称
-    author_email=about["__author_email__"],  # 作者邮箱
-    url=about["__url__"],   # 项目官网
-    packages=packages,    # 项目需要的包
-    data_files=file_data,   # 打包时需要打包的数据文件，如图片，配置文件等
+    name=NAME,  # 包名称
+    version=about['__version__'],  # 包版本
+    description=DESCRIPTION,  # 包详细描述
+    long_description=long_description,  # 长描述，通常是readme，打包到PiPy需要
+    long_description_content_type='text/markdown',
+    author=AUTHOR,  # 作者名称
+    author_email=EMAIL,  # 作者邮箱
+    python_requires=REQUIRES_PYTHON,  # Python版本依赖
+    url=URL,  # 项目官网
+    packages=find_packages(exclude=["tests", "*.tests", "*.tests.*", "tests.*"]),  # 项目需要的包
+    # If your package is a single module, use this instead of 'packages':
+    # py_modules=['mypackage'],
+
+    # entry_points={
+    #     'console_scripts': ['mycli=mymodule:cli'],
+    # },
+    install_requires=REQUIRED,  # 第三方库依赖
+    extras_require=EXTRAS,
+    # data_files=file_data,  # 打包时需要打包的数据文件，如图片，配置文件等
     include_package_data=True,  # 是否需要导入静态数据文件
-    python_requires=">=3.0, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3*",  # Python版本依赖
-    install_requires=requires,  # 第三方库依赖
-    zip_safe=False,  # 此项需要，否则卸载时报windows error
-    classifiers=[    # 程序的所属分类列表
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Natural Language :: English',
+    license='MIT',
+    classifiers=[  # 程序的所属分类列表
+        # Trove classifiers
+        # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
+        'License :: OSI Approved :: MIT License',
+        # 'Development Status :: 5 - Production/Stable',
+        # 'Intended Audience :: Developers',
+        # 'Natural Language :: English',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
+        # 'Programming Language :: Python :: 3.4',
+        # 'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
+        # 'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: Implementation :: CPython',
         'Programming Language :: Python :: Implementation :: PyPy'
     ],
+    # $ setup.py publish support.
+    cmdclass={
+        'upload': UploadCommand,
+    },
+    # zip_safe=False,  # 此项需要，否则卸载时报windows error
+
 )
